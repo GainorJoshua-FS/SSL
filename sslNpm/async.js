98bb70e5e2a31a0e51082d0b11c49d959b0f0145
GainorJoshua-FS <JQGainor@student.fullsail.edu>
2021-11-11 17:47:06 -0500

    sync/Async done

---
+++ sslNpm/async.js
@@ -0,0 +1,34 @@
+var http = require('http'); //change htt to http
+var myname = function(){ //Fixed spelling error in function
+    console.log("Here is my IP address");
+}
+
+async function callHttpbin() { //added n at the end to match the call later AND made it an async funtion
+    let promise = new Promise((resolve, reject) => {
+        http.get(
+            'http://httpbin.org/ip',
+            function(response) {
+                var str="";
+                response.setEncoding('utf8');
+                response.on('data', function(data){
+                    str += data;
+                });
+                response.on('end', function() {
+                    var result = JSON.parse(str);
+                    myips = result.origin;
+                    resolve(myips) //added myips to ()s
+                });
+            }
+            );
+        });
+        
+        let result = await promise;
+        return result; //added return
+    }
+executeAsyncTask()//Added a function call
+
+async function executeAsyncTask(){ //Made it an async function to use await
+    const valueA = await callHttpbin()
+    const valueB = myname();
+    console.log(valueB+" "+valueA)
+} //Added closing bracket
